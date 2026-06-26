9ec78ba220fa77f15b66bfbe7ce798397704691c
Alrifat26 <ronykhan4431@gmail.com>
2026-06-27 05:25:41 +0600

    Temporarily keep only Hero to debug homepage

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,21 +1,5 @@
 import Hero from "../../components/Hero";
- import FeaturedTasks from "../../components/FeaturedTasks";
- import TopFreelancers from "../../components/TopFreelancers";
- import HowItWorks from "../../components/HowItWorks";
- import PlatformStats from "../../components/PlatformStats";
- import PopularCategories from "../../components/PopularCategories";
- import Testimonials from "../../components/Testimonials";
-export default function Home() {
-  return (
-    <>
-      <Hero />
-      <FeaturedTasks />
-      <TopFreelancers />
-      <HowItWorks />
-      <PlatformStats />
-      <PopularCategories />
-      <Testimonials />
 
-    </>
-  );
+export default function Home() {
+  return <Hero />;
 }
